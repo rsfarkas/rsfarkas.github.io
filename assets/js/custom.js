f35e2e12f5bdbdcbf2ea313aa3b03931ddd3984a
rsfarkas <roxannesfarkas@gmail.com>
2015-12-03 21:33:09 -0500

    WIP success checking

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -179,9 +179,7 @@
 						$('#contact-form button').append('<i class="fa fa-cog fa-spin"></i> Wait...');
 					},
 					success: function(result) {
-						debugger;
-						console.log(result);
-						if(result.status == "SUCCESS") {
+						if(result.type == 'success') {
 							$('#contact-form .ajax-hidden').fadeOut(500);
 							responseMessage.html(result.message).fadeIn(500);
 						} else {
