98f7bafd66c4118a095844bdcd2f10569979c551
rsfarkas <roxannesfarkas@gmail.com>
2015-12-09 20:06:07 -0500

    testing background issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -65,11 +65,10 @@
         clientId: '8729b10b0f41420b9481f93f98a89fba',
         accessToken:'227009377.1677ed0.d52c27479e4f4ac0ac3a50c7a8ad8206',
         limit: '5',
-        template: '<a href="{{link}}"><img src="{{image}}" class="instaImg" height="{{height}}" width="{{width}}"/></a>'
+        template:'<a href="{{link}}"><img src="{{image}}" class="instaImg" height="{{height}}" width="{{width}}"/></a>'
     });
 
     feed.run();
-
 
 		/* ---------------------------------------------- /*
 		 * Initialize shuffle plugin
